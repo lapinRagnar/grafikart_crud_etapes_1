b249c5efa5546153f5c641a8d92ce9a64fcb844c
LapinRagnar <lapinragnar@gmail.com>
2020-05-23 14:14:53 +0200

    creation du CRUD jusqu'au def create

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,7 @@
 Rails.application.routes.draw do
+
+  get 'posts/index'
+  root to: 'home#home', as: 'home'
+  resources :posts
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
